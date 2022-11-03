5bb46bba8d1aa7c6b24e84284861d23f2a1ea9f0
suraj1724 <113453491+suraj1724@users.noreply.github.com>
2022-11-04 00:58:33 +0530

    password
    verfivation and validation

---
+++ index.js
@@ -0,0 +1,48 @@
+
+var catogories={
+    username:"suraj@1724",
+    password:"@1724"
+
+}
+var msge=document.getElementById("msg");
+var user = document.getElementById("usern").value;
+function userid(){
+    if(catogories.username===user){
+        msge.innerHTML="";
+
+    }
+    else{
+        msge.innerHTML="Invalid UserName";
+        msge.style.color="red";
+    }
+}
+1
+var attempt=4;
+function loginclick(){
+
+    
+    var password=document.getElementById("passn").value;
+    var invalidmsg=document.getElementById("invalid");
+    var passwor=document.getElementById("passn");
+  
+   
+   if(catogories.password===password){
+    invalidmsg.innerHTML=""
+    
+   }
+   else{
+    if(attempt==1){
+        invalidmsg.innerHTML="Your Account Is Blocked"
+        passwor.disabled=true;
+        invalidmsg.style.color="red";
+    }
+    else{
+        attempt=attempt-1;
+        invalidmsg.innerHTML=`invalid password ${attempt} attempt remain`
+
+
+    }
+}
+
+}                    
+
